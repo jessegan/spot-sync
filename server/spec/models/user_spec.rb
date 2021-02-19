e01bc73154eb48ccdf7f8dc1252851701ff852cc
jessegan <jesse13579@gmail.com>
2021-02-19 18:52:26 -0500

    add User model validations tests

--- server/spec/models/user_spec.rb
+++ server/spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Validations" do 
+
+    let(:valid_attributes) { { display_name: "test user", spotify_link: "www.test.com" } }
+    let(:missing_display_name) { { spotify_link: "www.test.com" } }
+    let(:missing_spotify_link) { { display_name: "test user" } }
+
+    it "is valid with valid attributes" do
+      user = User.new(valid_attributes)
+
+      expect(user).to be_valid
+    end
+    
+    it "is not valid wihtout a display_name" do
+      user = User.new(missing_display_name)
+
+      expect(user).to be_valid
+    end
+
+    it "is not valid without a spotify_link" do
+      user = User.new(missing_spotify_link)
+
+      expect(user).to be_valid
+    end
+
+  end
+
 end
